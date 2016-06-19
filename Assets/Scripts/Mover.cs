d509554e7709ad6c248bc8db93bcebc5e922b1ae
Nour_Ayad <nour6666@gmail.com>
2016-06-19 14:48:43 -0700

    Lesson 6 - Creating Bullets/Shots
    Created the bolt prefab, and the mover script that moves it. Bolt now
    has a laser texture.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody objectRigidBody;
+    public float speed;
+
+    void Start()
+    {
+        objectRigidBody = GetComponent<Rigidbody>();
+        objectRigidBody.velocity = transform.forward * speed;
+    }
+}

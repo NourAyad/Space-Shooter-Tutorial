136f491f1baeb15082f4523c28a8916d9f09e652
Nour_Ayad <nour6666@gmail.com>
2016-08-30 00:22:04 -0700

    Lesson 3.2 Counting and Displaying the Score
    Added a scoreboard to display the text, and modified the GameController
    script to control score. Also modified the DestroyByContact script to
    have a score value.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,6 +5,22 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -19,5 +35,6 @@ public class DestroyByContact : MonoBehaviour
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
+        gameController.AddScore(scoreValue);
     }
 }

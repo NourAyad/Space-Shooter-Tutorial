fcda4f39b3ee791fd0e410b5a925a91cb2ca0c6f
Nour_Ayad <nour6666@gmail.com>
2016-09-02 12:56:05 -0700

    Extending the Space Shooter
    Added 3 types of asteroids, an enemy ship that dodges, a scrolling
    background, a starfield, and more! Plus began work on converting to
    mobile.

---
+++ Assets/Scripts/EvasionManeuver.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvasionManeuver : MonoBehaviour {
+
+
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+
+    private float currentSpeed;
+    private Rigidbody rigidBody;
+    private float targetManeuver;
+	
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        currentSpeed = rigidBody.velocity.z;
+        StartCoroutine(Evade());
+	}
+
+    IEnumerator Evade ()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+        while (true)
+        {
+            targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+	
+	
+	void FixedUpdate ()
+    {
+        float newManeuver = Mathf.MoveTowards(rigidBody.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rigidBody.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+        rigidBody.position = new Vector3
+            (
+            Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * -tilt);
+
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,6 +43,7 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         objectRigidBody.velocity = movement * speed;
 
